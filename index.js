70e43dc1e6821fa6e05db10d685780fe7b80f6cb
Brenda Ferrufino <ferrufinob@gmail.com>
2021-03-17 19:05:12 -0400

    commit tree 1fd8a4e5fd613d427aebcaba099dfd264e9b0b12

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (driversArray) => {
+  return driversArray.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (driversArray) => {
+  return driversArray.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// Higher-order function 1.
+// function that returns a function
+function createFareMultiplier(integer) {
+  return function (fare) {
+    return fare * integer;
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(driversArray, func) {
+  return func(driversArray);
+}
